af7c8040328518299ffdc588b26a4bc4067099d5
daddykhant <khantthihazaw2018@gmail.com>
2025-01-27 11:57:24 +0630

    remove url

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.CLIENT_URL || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
