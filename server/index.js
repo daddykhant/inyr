9bbaa9e86af9739da042b1ef1441c5d60d20b36e
daddykhant <khantthihazaw2018@gmail.com>
2025-01-27 11:22:29 +0630

    index changes

--- server/index.js
+++ server/index.js
@@ -4,22 +4,21 @@ const bodyParser = require("body-parser");
 const bookRoutes = require("./routes/bookRoutes");
 const authRoutes = require("./routes/authRoutes");
 const cors = require("cors");
-const app = express();
 require("dotenv").config();
+
+const app = express();
 
 // Connect to MongoDB
 connectDB();
 
 // Define allowed origins
-const allowedOrigins = [
-  "https://inyr.vercel.app/", // frontend origin
-];
+const allowedOrigins = ["https://inyr.vercel.app"]; // frontend origin
 
 // CORS middleware
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or Postman requests)
+      // Allow requests with no origin (like Postman or curl)
       if (!origin || allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
@@ -27,16 +26,30 @@ app.use(
         "The CORS policy for this site does not allow access from the specified origin.";
       return callback(new Error(msg), false);
     },
-    credentials: true, // Allow cookies or HTTP authentication if needed
-    optionsSuccessStatus: 204, // For legacy browser support
+    credentials: true, // Allow cookies or HTTP authentication
   })
 );
 
+// Middleware for parsing JSON bodies
 app.use(bodyParser.json());
 
 // Routes
 app.use("/api/books", bookRoutes);
 app.use("/api/auth", authRoutes);
 
+// Test route
+app.get("/", (req, res) => {
+  res.status(200).send("API is running.");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).json({ message: "Internal server error" });
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
