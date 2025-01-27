21058c5ae98a0d1bba888aab3184645cce4da693
daddykhant <khantthihazaw2018@gmail.com>
2025-01-27 11:13:10 +0630

    port change

--- server/index.js
+++ server/index.js
@@ -38,5 +38,5 @@ app.use(bodyParser.json());
 app.use("/api/books", bookRoutes);
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
